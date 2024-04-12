6fd0b2138129303a83b0067d76e10f2b05d50687
seunghyonpark <creath.park@gmail.com>
2024-04-12 14:33:46 +0900

    update mbc apk

--- lib/hooks/use-streamable-text.ts
+++ lib/hooks/use-streamable-text.ts
@@ -9,17 +9,21 @@ export const useStreamableText = (
   )
 
   useEffect(() => {
+
     ;(async () => {
       if (typeof content === 'object') {
         let value = ''
         for await (const delta of readStreamableValue(content)) {
+
           console.log(delta)
+          
           if (typeof delta === 'string') {
             setRawContent((value = value + delta))
           }
         }
       }
     })()
+
   }, [content])
 
   return rawContent
