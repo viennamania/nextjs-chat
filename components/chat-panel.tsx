f480386f83bc080ef285fa0dfba848329815ef75
seunghyonpark <creath.park@gmail.com>
2024-07-14 01:04:26 +0900

    three some

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -35,6 +35,7 @@ export function ChatPanel({
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
 
   const exampleMessages = [
+    /*
     {
       heading: 'What are the',
       subheading: 'trending memecoins today?',
@@ -55,7 +56,10 @@ export function ChatPanel({
       subheading: `recent events about $DOGE?`,
       message: `What are some recent events about $DOGE?`
     }
-  ]
+    */
+
+  ] as any;
+
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
@@ -67,7 +71,8 @@ export function ChatPanel({
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
-            exampleMessages.map((example, index) => (
+            ///exampleMessages.map((example, index) => (
+            exampleMessages.map((example : any, index : number) => (
               <div
                 key={example.heading}
                 className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
